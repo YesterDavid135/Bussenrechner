7f7f115109f7bfa67e73f64dc8cc1e92dc72133c
yesterdavid135 <david.minder@sunrise.ch>
2023-08-21 22:23:08 +0200

    Added Tolerance Calculation

--- src/app/speeding/speeding.component.ts
+++ src/app/speeding/speeding.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-speeding',
@@ -9,22 +9,67 @@ export class SpeedingComponent {
 
   roadType: string = "";
   speedLimit: number = 0;
-
+  type: string = "";
+  speed: any;
+  tolerance: number = 0;
 
   getAvailableSpeeds(): number[] {
-    this.speedLimit = 0;
-    if (this.roadType === "innerorts"){
-      return [20,30,40,50,60];
+    if (this.roadType === "innerorts") {
+      return [20, 30, 40, 50, 60];
     }
-    if (this.roadType === "ausserorts"){
+    if (this.roadType === "ausserorts") {
       return [50, 60, 70, 80]
     }
-    if (this.roadType === "autostrasse"){
+    if (this.roadType === "autostrasse") {
       return [60, 70, 80, 100]
     }
-    if (this.roadType === "autobahn"){
+    if (this.roadType === "autobahn") {
       return [60, 70, 80, 100, 120]
     }
     return [];
+  }
+
+  calculate() {
+    if (!this.roadType || !this.speedLimit || !this.type || !this.speed) {
+      console.warn("Please use all fields");
+      return;
+    }
+    if (this.speedLimit > this.speed) {
+      console.warn("You are not speeding");
+      return;
+    }
+
+    this.calculateTolerance()
+
+  }
+
+  private calculateTolerance() {
+    switch (this.type) {
+      case "radar":
+      case "section":
+        if (this.speed <= 100) {
+          this.tolerance = 5;
+        } else if (this.speed <= 150) {
+          this.tolerance = 6;
+        } else {
+          this.tolerance = 7;
+        }
+        break;
+      case "laser":
+        if (this.speed <= 100) {
+          this.tolerance = 3;
+        } else if (this.speed <= 150) {
+          this.tolerance = 4;
+        } else {
+          this.tolerance = 5;
+        }
+        break;
+      case "curve":
+        if (this.speed <= 100) {
+          this.tolerance = 10;
+        } else {
+          this.tolerance = 14;
+        }
+    }
   }
 }
