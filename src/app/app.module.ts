6c3b6061c67fb28b2adb7ffceeb0540f18e63d9f
YesterDavid135 <hello@ydavid.ch>
2024-04-23 13:48:17 +0200

    Added google analytics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { SpeedingComponent } from './speeding/speeding.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { NavbarComponent } from './navbar/navbar.component';
+import {AppComponent} from './app.component';
+import {SpeedingComponent} from './speeding/speeding.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {NavbarComponent} from './navbar/navbar.component';
 import {FormsModule} from "@angular/forms";
-import { FooterComponent } from './footer/footer.component';
+import {FooterComponent} from './footer/footer.component';
+import {NgxGoogleAnalyticsModule} from "ngx-google-analytics";
 
 
 @NgModule({
@@ -16,12 +17,14 @@ import { FooterComponent } from './footer/footer.component';
     NavbarComponent,
     FooterComponent
   ],
-    imports: [
-        BrowserModule,
-        NgbModule,
-        FormsModule
-    ],
+  imports: [
+    BrowserModule,
+    NgbModule,
+    FormsModule,
+    NgxGoogleAnalyticsModule.forRoot('G-DPZJPVGHM2'),
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
